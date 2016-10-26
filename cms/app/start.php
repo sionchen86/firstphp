73b431d293505d4bcc37d2a2a9a4c3cc7070a439
Sion Chen <sionchen@outlook.com>
2016-10-26 16:00:34 +0200

    upload simple CMS files

---
+++ cms/app/start.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 'On');
+
+define('APP_ROOT', __DIR__);
+define('VIEW_ROOT', APP_ROOT . '/views');
+define('BASE_URL', 'http://localhost/cms');
+
+$db = new PDO('mysql:host=localhost;dbname=cms','root', '');
+?>
